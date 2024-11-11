bfbaaf41d06d6926bbbecf0e80d4aded7bf724f6
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-11-11 16:08:37 -0500

    Fix image sizing

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -1,198 +1,207 @@
 ﻿namespace TicTacToe
 {
-	partial class formTicTacToe
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class formTicTacToe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			pb1 = new PictureBox();
-			pb2 = new PictureBox();
-			pb3 = new PictureBox();
-			pb4 = new PictureBox();
-			pb5 = new PictureBox();
-			pb6 = new PictureBox();
-			pb7 = new PictureBox();
-			pb8 = new PictureBox();
-			pb9 = new PictureBox();
-			lblHeading = new Label();
-			((System.ComponentModel.ISupportInitialize)pb1).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb2).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb3).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb4).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb5).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb6).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb7).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb8).BeginInit();
-			((System.ComponentModel.ISupportInitialize)pb9).BeginInit();
-			SuspendLayout();
-			// 
-			// pb1
-			// 
-			pb1.BorderStyle = BorderStyle.FixedSingle;
-			pb1.Location = new Point(171, 89);
-			pb1.Name = "pb1";
-			pb1.Size = new Size(80, 80);
-			pb1.TabIndex = 0;
-			pb1.TabStop = false;
-			pb1.Click += UpdateBox;
-			// 
-			// pb2
-			// 
-			pb2.BorderStyle = BorderStyle.FixedSingle;
-			pb2.Location = new Point(248, 89);
-			pb2.Name = "pb2";
-			pb2.Size = new Size(80, 80);
-			pb2.TabIndex = 1;
-			pb2.TabStop = false;
-			pb2.Click += UpdateBox;
-			// 
-			// pb3
-			// 
-			pb3.BorderStyle = BorderStyle.FixedSingle;
-			pb3.Location = new Point(325, 89);
-			pb3.Name = "pb3";
-			pb3.Size = new Size(80, 80);
-			pb3.TabIndex = 2;
-			pb3.TabStop = false;
-			pb3.Click += UpdateBox;
-			// 
-			// pb4
-			// 
-			pb4.BorderStyle = BorderStyle.FixedSingle;
-			pb4.Location = new Point(171, 168);
-			pb4.Name = "pb4";
-			pb4.Size = new Size(80, 80);
-			pb4.TabIndex = 3;
-			pb4.TabStop = false;
-			pb4.Click += UpdateBox;
-			// 
-			// pb5
-			// 
-			pb5.BorderStyle = BorderStyle.FixedSingle;
-			pb5.Location = new Point(248, 168);
-			pb5.Name = "pb5";
-			pb5.Size = new Size(80, 80);
-			pb5.TabIndex = 4;
-			pb5.TabStop = false;
-			pb5.Click += UpdateBox;
-			// 
-			// pb6
-			// 
-			pb6.BackgroundImageLayout = ImageLayout.Center;
-			pb6.BorderStyle = BorderStyle.FixedSingle;
-			pb6.Location = new Point(325, 168);
-			pb6.Name = "pb6";
-			pb6.Size = new Size(80, 80);
-			pb6.TabIndex = 5;
-			pb6.TabStop = false;
-			pb6.Click += UpdateBox;
-			// 
-			// pb7
-			// 
-			pb7.BorderStyle = BorderStyle.FixedSingle;
-			pb7.Location = new Point(171, 245);
-			pb7.Name = "pb7";
-			pb7.Size = new Size(80, 80);
-			pb7.TabIndex = 6;
-			pb7.TabStop = false;
-			pb7.Click += UpdateBox;
-			// 
-			// pb8
-			// 
-			pb8.BorderStyle = BorderStyle.FixedSingle;
-			pb8.Location = new Point(248, 245);
-			pb8.Name = "pb8";
-			pb8.Size = new Size(80, 80);
-			pb8.TabIndex = 7;
-			pb8.TabStop = false;
-			pb8.Click += UpdateBox;
-			// 
-			// pb9
-			// 
-			pb9.BorderStyle = BorderStyle.FixedSingle;
-			pb9.Location = new Point(325, 245);
-			pb9.Name = "pb9";
-			pb9.Size = new Size(80, 80);
-			pb9.TabIndex = 8;
-			pb9.TabStop = false;
-			pb9.Click += UpdateBox;
-			// 
-			// lblHeading
-			// 
-			lblHeading.AutoSize = true;
-			lblHeading.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-			lblHeading.ForeColor = Color.White;
-			lblHeading.Location = new Point(209, 34);
-			lblHeading.Name = "lblHeading";
-			lblHeading.Size = new Size(145, 29);
-			lblHeading.TabIndex = 9;
-			lblHeading.Text = "Tic Tac Toe";
-			// 
-			// formTicTacToe
-			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
-			AutoScaleMode = AutoScaleMode.Font;
-			BackColor = Color.Black;
-			ClientSize = new Size(586, 377);
-			Controls.Add(lblHeading);
-			Controls.Add(pb9);
-			Controls.Add(pb8);
-			Controls.Add(pb7);
-			Controls.Add(pb6);
-			Controls.Add(pb5);
-			Controls.Add(pb4);
-			Controls.Add(pb3);
-			Controls.Add(pb2);
-			Controls.Add(pb1);
-			Name = "formTicTacToe";
-			Text = "Tic Tac Toe";
-			((System.ComponentModel.ISupportInitialize)pb1).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb2).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb3).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb4).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb5).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb6).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb7).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb8).EndInit();
-			((System.ComponentModel.ISupportInitialize)pb9).EndInit();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pb1 = new PictureBox();
+            pb2 = new PictureBox();
+            pb3 = new PictureBox();
+            pb4 = new PictureBox();
+            pb5 = new PictureBox();
+            pb6 = new PictureBox();
+            pb7 = new PictureBox();
+            pb8 = new PictureBox();
+            pb9 = new PictureBox();
+            lblHeading = new Label();
+            ((System.ComponentModel.ISupportInitialize)pb1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pb9).BeginInit();
+            SuspendLayout();
+            // 
+            // pb1
+            // 
+            pb1.BorderStyle = BorderStyle.FixedSingle;
+            pb1.Location = new Point(171, 89);
+            pb1.Name = "pb1";
+            pb1.Size = new Size(80, 80);
+            pb1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb1.TabIndex = 0;
+            pb1.TabStop = false;
+            pb1.Click += UpdateBox;
+            // 
+            // pb2
+            // 
+            pb2.BorderStyle = BorderStyle.FixedSingle;
+            pb2.Location = new Point(248, 89);
+            pb2.Name = "pb2";
+            pb2.Size = new Size(80, 80);
+            pb2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb2.TabIndex = 1;
+            pb2.TabStop = false;
+            pb2.Click += UpdateBox;
+            // 
+            // pb3
+            // 
+            pb3.BorderStyle = BorderStyle.FixedSingle;
+            pb3.Location = new Point(325, 89);
+            pb3.Name = "pb3";
+            pb3.Size = new Size(80, 80);
+            pb3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb3.TabIndex = 2;
+            pb3.TabStop = false;
+            pb3.Click += UpdateBox;
+            // 
+            // pb4
+            // 
+            pb4.BorderStyle = BorderStyle.FixedSingle;
+            pb4.Location = new Point(171, 168);
+            pb4.Name = "pb4";
+            pb4.Size = new Size(80, 80);
+            pb4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb4.TabIndex = 3;
+            pb4.TabStop = false;
+            pb4.Click += UpdateBox;
+            // 
+            // pb5
+            // 
+            pb5.BorderStyle = BorderStyle.FixedSingle;
+            pb5.Location = new Point(248, 168);
+            pb5.Name = "pb5";
+            pb5.Size = new Size(80, 80);
+            pb5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb5.TabIndex = 4;
+            pb5.TabStop = false;
+            pb5.Click += UpdateBox;
+            // 
+            // pb6
+            // 
+            pb6.BackgroundImageLayout = ImageLayout.Center;
+            pb6.BorderStyle = BorderStyle.FixedSingle;
+            pb6.Location = new Point(325, 168);
+            pb6.Name = "pb6";
+            pb6.Size = new Size(80, 80);
+            pb6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb6.TabIndex = 5;
+            pb6.TabStop = false;
+            pb6.Click += UpdateBox;
+            // 
+            // pb7
+            // 
+            pb7.BorderStyle = BorderStyle.FixedSingle;
+            pb7.Location = new Point(171, 245);
+            pb7.Name = "pb7";
+            pb7.Size = new Size(80, 80);
+            pb7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb7.TabIndex = 6;
+            pb7.TabStop = false;
+            pb7.Click += UpdateBox;
+            // 
+            // pb8
+            // 
+            pb8.BorderStyle = BorderStyle.FixedSingle;
+            pb8.Location = new Point(248, 245);
+            pb8.Name = "pb8";
+            pb8.Size = new Size(80, 80);
+            pb8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb8.TabIndex = 7;
+            pb8.TabStop = false;
+            pb8.Click += UpdateBox;
+            // 
+            // pb9
+            // 
+            pb9.BorderStyle = BorderStyle.FixedSingle;
+            pb9.Location = new Point(325, 245);
+            pb9.Name = "pb9";
+            pb9.Size = new Size(80, 80);
+            pb9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb9.TabIndex = 8;
+            pb9.TabStop = false;
+            pb9.Click += UpdateBox;
+            // 
+            // lblHeading
+            // 
+            lblHeading.AutoSize = true;
+            lblHeading.Font = new Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHeading.ForeColor = Color.White;
+            lblHeading.Location = new Point(209, 34);
+            lblHeading.Name = "lblHeading";
+            lblHeading.Size = new Size(145, 29);
+            lblHeading.TabIndex = 9;
+            lblHeading.Text = "Tic Tac Toe";
+            // 
+            // formTicTacToe
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(586, 377);
+            Controls.Add(lblHeading);
+            Controls.Add(pb9);
+            Controls.Add(pb8);
+            Controls.Add(pb7);
+            Controls.Add(pb6);
+            Controls.Add(pb5);
+            Controls.Add(pb4);
+            Controls.Add(pb3);
+            Controls.Add(pb2);
+            Controls.Add(pb1);
+            Name = "formTicTacToe";
+            Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)pb1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pb9).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private PictureBox pb1;
-		private PictureBox pb2;
-		private PictureBox pb3;
-		private PictureBox pb4;
-		private PictureBox pb5;
-		private PictureBox pb6;
-		private PictureBox pb7;
-		private PictureBox pb8;
-		private PictureBox pb9;
-		private Label lblHeading;
-	}
+        private PictureBox pb1;
+        private PictureBox pb2;
+        private PictureBox pb3;
+        private PictureBox pb4;
+        private PictureBox pb5;
+        private PictureBox pb6;
+        private PictureBox pb7;
+        private PictureBox pb8;
+        private PictureBox pb9;
+        private Label lblHeading;
+    }
 }
--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -9,166 +9,166 @@
  */
 namespace TicTacToe
 {
-	public partial class formTicTacToe : Form
-	{
-		// Setting starting choice(X)
-		private char currentChoice = 'X';
-		private int turnsPlayed = 0;
+    public partial class formTicTacToe : Form
+    {
+        // Setting starting choice(X)
+        private char currentChoice = 'X';
+        private int turnsPlayed = 0;
 
-		private void RestartGame()
-		{
-			// Clear picture boxes
-			foreach (Control c in Controls)
-			{
-				if (c is PictureBox)
-				{
-					PictureBox pb = (PictureBox)c;
-					pb.Image = null;
-					pb.Name = pb.Name.Remove(3); // Removes X or O prefix from the name of picture box
-					pb.Enabled = true; // Again enabling all the PictureBoxes
-				}
-			}
+        private void RestartGame()
+        {
+            // Clear picture boxes
+            foreach (Control c in Controls)
+            {
+                if (c is PictureBox)
+                {
+                    PictureBox pb = (PictureBox)c;
+                    pb.Image = null;
+                    pb.Name = pb.Name.Remove(3); // Removes X or O prefix from the name of picture box
+                    pb.Enabled = true; // Again enabling all the PictureBoxes
+                }
+            }
 
-			// Setting current choice back to 'X'
-			currentChoice = 'X';
-			turnsPlayed = 0;
-		}
+            // Setting current choice back to 'X'
+            currentChoice = 'X';
+            turnsPlayed = 0;
+        }
 
-		// Get the picture path based on current choice
-		private Bitmap GetCurrentImage()
-		{
-			return (currentChoice == 'X') ?
-				Properties.Resources.X :
-				Properties.Resources.O;
-		}
+        // Get the picture path based on current choice
+        private Bitmap GetCurrentImage()
+        {
+            return (currentChoice == 'X') ?
+                Properties.Resources.X :
+                Properties.Resources.O;
+        }
 
-		private bool checkMatch(int[,] pattern)
-		{
-			// Create a 3D array to store all the possible winning match cases 
-			int[,,] matchCases =
-			{
+        private bool checkMatch(int[,] pattern)
+        {
+            // Create a 3D array to store all the possible winning match cases 
+            int[,,] matchCases =
+            {
 				// Rows
 				{
-					{1, 1, 1 },
-					{0, 0, 0 },
-					{0, 0, 0 },
-				},
-				{
+                    {1, 1, 1 },
+                    {0, 0, 0 },
+                    {0, 0, 0 },
+                },
+                {
 
-					{0, 0, 0 },
-					{1, 1, 1 },
-					{0, 0, 0 },
+                    {0, 0, 0 },
+                    {1, 1, 1 },
+                    {0, 0, 0 },
 
-				},
-				{
-					{0, 0, 0 },
-					{0, 0, 0 },
-					{1, 1, 1 },
-				},
+                },
+                {
+                    {0, 0, 0 },
+                    {0, 0, 0 },
+                    {1, 1, 1 },
+                },
 
 					// Columns
 				{
-					{1, 0, 0 },
-					{1, 0, 0 },
-					{1, 0, 0 },
-				},
-				{
-					{0, 1, 0 },
-					{0, 1, 0 },
-					{0, 1, 0 },
-				},
-				{
-					{0, 0, 1 },
-					{0, 0, 1 },
-					{0, 0, 1 },
-				},
+                    {1, 0, 0 },
+                    {1, 0, 0 },
+                    {1, 0, 0 },
+                },
+                {
+                    {0, 1, 0 },
+                    {0, 1, 0 },
+                    {0, 1, 0 },
+                },
+                {
+                    {0, 0, 1 },
+                    {0, 0, 1 },
+                    {0, 0, 1 },
+                },
 
 					// Diagonals
 				{
-					{1, 0, 0 },
-					{0, 1, 0 },
-					{0, 0, 1 },
-				},
+                    {1, 0, 0 },
+                    {0, 1, 0 },
+                    {0, 0, 1 },
+                },
 
-				{
-					{0, 0, 1 },
-					{0, 1, 0 },
-					{1, 0, 0 },
-				}
-			};
+                {
+                    {0, 0, 1 },
+                    {0, 1, 0 },
+                    {1, 0, 0 },
+                }
+            };
 
-			for (int i = 0; i < matchCases.GetLength(0); i++)
-			{
-				bool matching = true;
-				for (int row = 0; row < matchCases.GetLength(1); row++)
-				{
-					for (int col = 0; col < matchCases.GetLength(2); col++)
-					{
-						if (matchCases[i, row, col] != pattern[row, col]
-						 && matchCases[i, row, col] == 1)
-						{
-							matching = false;
-							break;
-						}
-					}
-					// Break the whole loop if even one element is unmatched and move on to the next case
-					if (!matching)
-					{
-						break;
-					}
-				}
-				// Check if this match case matched the pattern
-				if (matching) return true;
-			}
-			return false;
-		}
+            for (int i = 0; i < matchCases.GetLength(0); i++)
+            {
+                bool matching = true;
+                for (int row = 0; row < matchCases.GetLength(1); row++)
+                {
+                    for (int col = 0; col < matchCases.GetLength(2); col++)
+                    {
+                        if (matchCases[i, row, col] != pattern[row, col]
+                         && matchCases[i, row, col] == 1)
+                        {
+                            matching = false;
+                            break;
+                        }
+                    }
+                    // Break the whole loop if even one element is unmatched and move on to the next case
+                    if (!matching)
+                    {
+                        break;
+                    }
+                }
+                // Check if this match case matched the pattern
+                if (matching) return true;
+            }
+            return false;
+        }
 
-		private int[,] createPattern(char symbol)
-		{
-			int[,] pattern = new int[3, 3];
-			int patternRow = 0;
-			int patternCol = 0;
+        private int[,] createPattern(char symbol)
+        {
+            int[,] pattern = new int[3, 3];
+            int patternRow = 0;
+            int patternCol = 0;
 
-			foreach (Control pb in Controls)
-			{
-				// The array gets created in reverse order but it does not matter because if there is a match, it is still going to be shown in the array
-				if (pb is PictureBox)
-				{
-					if (pb.Name.EndsWith(symbol)) pattern[patternRow, patternCol] = 1;
-					else pattern[patternRow, patternCol] = 0;
-					if (patternCol++ == 2) { patternRow++; patternCol = 0; }
-				}
-			}
-			return pattern;
-		}
-		private void UpdateBox(object sender, EventArgs e)
-		{
-			PictureBox box = (PictureBox)sender;
-			box.Enabled = false;
-			turnsPlayed++;
-			// Changing the currentChoice because it will change every time this function is called to set the choice
-			box.Image = GetCurrentImage();
-			box.Name += currentChoice; // Adds the symbol which the current box is holding to its name
-									   // Game Logic
-			if (checkMatch(createPattern(currentChoice)) == true) // check for matching winning patterns
-			{
-				DialogResult result = MessageBox.Show($"{currentChoice} wins the game!", "Tic Tac Toe", MessageBoxButtons.RetryCancel);
-				if (result == DialogResult.Retry) RestartGame();
-				else Application.Exit();
-				return;
-			}
-			if (turnsPlayed >= 9)
-			{
-				DialogResult result = MessageBox.Show($"It is a Tie!", "Tic Tac Toe", MessageBoxButtons.RetryCancel);
-				if (result == DialogResult.Retry) RestartGame();
-				else Application.Exit();
-				return;
-			}
-			currentChoice = (currentChoice == 'X') ? 'O' : 'X';
-		}
-		public formTicTacToe()
-		{
-			InitializeComponent();
-		}
-	}
+            foreach (Control pb in Controls)
+            {
+                // The array gets created in reverse order but it does not matter because if there is a match, it is still going to be shown in the array
+                if (pb is PictureBox)
+                {
+                    if (pb.Name.EndsWith(symbol)) pattern[patternRow, patternCol] = 1;
+                    else pattern[patternRow, patternCol] = 0;
+                    if (patternCol++ == 2) { patternRow++; patternCol = 0; }
+                }
+            }
+            return pattern;
+        }
+        private void UpdateBox(object sender, EventArgs e)
+        {
+            PictureBox box = (PictureBox)sender;
+            box.Enabled = false;
+            turnsPlayed++;
+            // Changing the currentChoice because it will change every time this function is called to set the choice
+            box.Image = GetCurrentImage();
+            box.Name += currentChoice; // Adds the symbol which the current box is holding to its name
+                                       // Game Logic
+            if (checkMatch(createPattern(currentChoice)) == true) // check for matching winning patterns
+            {
+                DialogResult result = MessageBox.Show($"{currentChoice} wins the game!", "Tic Tac Toe", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Retry) RestartGame();
+                else Application.Exit();
+                return;
+            }
+            if (turnsPlayed >= 9)
+            {
+                DialogResult result = MessageBox.Show($"It is a Tie!", "Tic Tac Toe", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Retry) RestartGame();
+                else Application.Exit();
+                return;
+            }
+            currentChoice = (currentChoice == 'X') ? 'O' : 'X';
+        }
+        public formTicTacToe()
+        {
+            InitializeComponent();
+        }
+    }
 }
